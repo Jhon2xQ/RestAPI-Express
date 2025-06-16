8f67434744f8c3db5d79c2b7a167dad3dd57f366
BeeEpisodios <jhonq2512@gmail.com>
2025-06-16 16:15:37 -0500

    implementando el repositorio para tb producto

--- src/infraestructure/persistence/producto.repository.ts
+++ src/infraestructure/persistence/producto.repository.ts
@@ -6,4 +6,29 @@ export class ProductoRepository implements IProductoRepository {
   async getAll(): Promise<Producto[]> {
     return prisma.producto.findMany();
   }
+
+  async getById(id: number): Promise<Producto | null> {
+    return prisma.producto.findUnique({
+      where: { id },
+    });
+  }
+
+  async create(producto: Producto): Promise<Producto> {
+    return prisma.producto.create({
+      data: producto,
+    });
+  }
+
+  async update(producto: Producto, id: number): Promise<Producto> {
+    return prisma.producto.update({
+      where: { id },
+      data: producto,
+    });
+  }
+
+  async delete(id: number): Promise<Producto> {
+    return prisma.producto.delete({
+      where: { id },
+    });
+  }
 }
