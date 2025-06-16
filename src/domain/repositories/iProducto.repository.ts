a26d50599de73f111ca373dead99027dca5e46ed
BeeEpisodios <jhonq2512@gmail.com>
2025-06-16 16:08:09 -0500

    interface del crud completado

--- src/domain/repositories/iProducto.repository.ts
+++ src/domain/repositories/iProducto.repository.ts
@@ -2,4 +2,8 @@ import { Producto } from "../entities/producto.entity";
 
 export interface IProductoRepository {
   getAll(): Promise<Producto[]>;
+  getById(id: number): Promise<Producto | null>;
+  create(producto: Producto): Promise<Producto>;
+  update(producto: Producto, id: number): Promise<Producto>;
+  delete(id: number): Promise<boolean>;
 }
