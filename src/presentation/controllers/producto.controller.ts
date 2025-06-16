abda53e0f57bfaa0e660454537e843dce363b368
BeeEpisodios <jhonq2512@gmail.com>
2025-06-16 17:24:16 -0500

    creacion de los controladores y rutas para tb Producto

--- src/presentation/controllers/producto.controller.ts
+++ src/presentation/controllers/producto.controller.ts
@@ -1,12 +1,73 @@
 import { Request, Response } from "express";
 import ProductoService from "../../application/services/producto.service";
+import { error } from "console";
 
 //falta el return y promise para devolver con middleware
 export default class ProductoController {
   constructor(private productoService: ProductoService) {}
 
-  getAll = async (_: Request, res: Response) => {
-    const productos = await this.productoService.getAll();
-    res.status(200).json({ productos });
+  getAll = async (_: Request, res: Response): Promise<Response<unknown>> => {
+    try {
+      const productos = await this.productoService.getAll();
+      return res.status(200).json({ productos });
+    } catch (error) {
+      return res.status(500).json({
+        error: {
+          code: 500,
+          message: "Internal server error",
+          details: error,
+        },
+      });
+    }
+  };
+
+  getById = async (req: Request, res: Response): Promise<Response<unknown>> => {
+    const producto = await this.productoService.getById(Number(req.params.id));
+    if (producto) {
+      return res.status(200).json({ producto });
+    } else {
+      return res.status(404).json({
+        error: {
+          code: 404,
+          message: "error al obtener producto",
+        },
+      });
+    }
+  };
+
+  create = async (req: Request, res: Response): Promise<Response<unknown>> => {
+    const producto = await this.productoService.create(req.body);
+    return res.status(201).json({ producto });
+  };
+
+  update = async (req: Request, res: Response): Promise<Response<unknown>> => {
+    const producto = await this.productoService.update(
+      req.body,
+      Number(req.params.id)
+    );
+    if (producto) {
+      return res.status(200).json({ producto });
+    } else {
+      return res.status(404).json({
+        error: {
+          code: 404,
+          message: "Error al actualizar producto",
+        },
+      });
+    }
+  };
+
+  delete = async (req: Request, res: Response): Promise<Response<unknown>> => {
+    const producto = await this.productoService.delete(Number(req.params.id));
+    if (producto) {
+      return res.status(200).json({ producto });
+    } else {
+      return res.status(404).json({
+        error: {
+          code: 404,
+          message: "Error al eliminar producto",
+        },
+      });
+    }
   };
 }
--- src/routes/producto.route.ts
+++ src/routes/producto.route.ts
@@ -8,6 +8,20 @@ const repo = new ProductoRepository();
 const productoService = new ProductoService(repo);
 const productoController = new ProductoController(productoService);
 
-productoRouter.get("/", productoController.getAll);
+productoRouter.get("/", (req, res) => {
+  productoController.getAll(req, res);
+});
+productoRouter.get("/:id", (req, res) => {
+  productoController.getById(req, res);
+});
+productoRouter.post("/", (req, res) => {
+  productoController.create(req, res);
+});
+productoRouter.put("/:id", (req, res) => {
+  productoController.update(req, res);
+});
+productoRouter.delete("/:id", (req, res) => {
+  productoController.delete(req, res);
+});
 
 export default productoRouter;
